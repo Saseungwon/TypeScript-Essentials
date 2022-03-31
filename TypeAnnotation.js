4d2f131804e8b458b0051e6f153f9dbccbe6d904
dfg1425@naver.com <dfg1425@naver.com>
2022-03-31 14:20:00 +0900

    TypeAnnotation

---
+++ TypeAnnotation.js
@@ -0,0 +1,8 @@
+"use strict";
+let a; // type annotation : 타입을 지정해준다. 
+a = "Mark";
+//a = 39;           // 오류
+function hello(b) {
+}
+hello(39);
+//hello("Mark");    // 오류
---
+++ TypeAnnotation.ts
@@ -0,0 +1,10 @@
+let a: string;    // type annotation : 타입을 지정해준다. 
+a = "Mark";
+//a = 39;           // 오류
+
+
+function hello(b: number){
+
+}
+hello(39);
+//hello("Mark");    // 오류
